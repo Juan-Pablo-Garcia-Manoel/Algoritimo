d0fca6b601e756a5684f5fa2a3626103303797cd
JuanPabloGarciaManoel <juan.manoel@viannasempre.com.br>
2024-02-26 01:09:05 -0300

    Ajuste

---
+++ VIANNA/01/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        /*Faça um programa que imprima o seu nome e sobrenome no formato (Sobrenome, Nome).*/
+        Console.WriteLine("Juan Pablo");
+        
+        /*Sabendo que a instrução Console.WriteLine("") imprime o 
+        texto que estiver entre aspas e salta a linha, faça um 
+        programa que escreva a inicial de seu nome utilizando 
+        sequências de espaços e letras X.*/
+        Console.WriteLine("XXXXXXXXXX");
+        Console.WriteLine("    XX    ");
+        Console.WriteLine("    XX    ");
+        Console.WriteLine(" X  XX    ");
+        Console.WriteLine(" XXXXX    ");
+    
+        /*Teste a seguinte instrução: 
+        Console.WriteLine("Teste\nde\nquebra\nde\nlinha"); 
+        Entendendo que \n faz com que o programa salte uma linha, 
+        reescreva o programa feito no exercício 2 de forma que o 
+        mesmo utilize somente um comando do tipo 
+        Console.WriteLine e um comando Console.ReadKey.*/
+        Console.WriteLine("Teste\nde\nquebra\nde\nlinha");
+        Console.WriteLine("XXXXXXXXXX\n    XX\n    XX\n X  XX\n XXXXX");
+        Console.ReadKey();
+
+    }
+}
---
+++ VIANNA/02/Program.cs
@@ -0,0 +1,83 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*Exemplo*/
+        int a;
+                float b;
+            float c;
+        
+        a = 0;
+        b = (float)2.5;
+        c = 8.7F * b;
+
+        //Console.WriteLine(c);
+
+        /*Declarando uma variavel*/
+        int idade;
+        float peso;
+        double altura;
+        char sexo;
+
+        /*Info*/
+        /*
+        * int = 4 bytes
+        * float = 4 bytes
+        * char = 2 bytes 'v'
+        * double =  8 bytes
+        */       
+
+        /*Declarando variável em uma declaração*/
+        int nomeVariavel = 25;
+
+        /*Operação aritiméticas
+        Math.Pow(base,2);
+        Math.Sqrt(16);
+        Math.Sin(x);
+        Math.Cos(x);
+        Math.Cos(2*x);
+        Math.Sin(x)*Math.Cos(y);
+        Math.Abs(-5);*/
+
+        /*Operadores
+        
+        == igual
+        != diferente
+        < menor que
+        <= menor ou igual a
+        > maior
+        >= maior ou igual a
+        ! negação(Não)
+        && conjunção(E)
+        || dijunção(Ou)
+        
+        */
+
+        idade = 25;
+        Console.Write("Idade: ");
+        Console.WriteLine(idade);
+
+        String nome = "Juan";
+        String Sobrenome = "Pablo";
+        Console.Write("Nome: {0} {1}",nome,Sobrenome);
+
+        /*
+            Prioridade para execução de operações em uma expressão:
+            1. Parênteses (dos mais internos para os mais externos);
+            2. Expressões aritméticas, seguindo a ordem: funções, multiplicação e divisão, adição e subtração;
+            3. Expressões lógicas relacionais: <, <=, ==, >, >= e !=;
+            4. Expressões lógicas, seguindo a ordem: negação, conjunção, disjunção; 
+            5. Da esquerda para a direita, quando houver indeterminações. Na dúvida, use parênteses em suas expressões.
+        */
+
+        /*
+            Letura de algum dado
+            int nomeVar = Convert.ToInt32(Console.ReadLine());
+            float nomeVar = Convert.ToSingle(Console.ReadLine());
+            double nomeVar = Convert.ToDouble(Console.ReadLine());     
+        */
+
+        
+
+    }
+}
