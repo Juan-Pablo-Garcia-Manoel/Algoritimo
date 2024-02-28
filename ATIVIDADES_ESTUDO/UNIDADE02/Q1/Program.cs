002d00a25110e75d14915fcd86550915481b9186
JuanPabloGarciaManoel <juan.manoel@viannasempre.com.br>
2024-02-28 04:38:38 -0300

    Estudando

---
+++ ATIVIDADES_ESTUDO/UNIDADE02/Q1/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+
+    /*
+    Faça um programa que lê uma temperatura em
+    graus Celsius e apresenta-a convertida em graus
+    Fahrenheit. A fórmula de conversão: F ← (9*C+160)/5
+    */
+    private static void Main(string[] args)
+    {
+        double C,F;
+
+        Console.Write("Informe a temperatura: ");
+        C = Convert.ToDouble(Console.ReadLine());
+        F = (9 * C + 160) / 5;
+
+        Console.WriteLine("{0} graus Celsius, equivale á {1} graus Fahrenheit",C,F);
+
+    }
+}
