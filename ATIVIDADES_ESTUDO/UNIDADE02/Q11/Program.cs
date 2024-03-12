b803e6fecca3e9fc5ef938cad7d80b22f1fb5eb8
JuanPabloGarciaManoel <79871430+JuanPabloGarciaManoel@users.noreply.github.com>
2024-03-12 22:01:04 +0000

    Estudo

--- ATIVIDADES_ESTUDO/UNIDADE02/Q11/Program.cs
+++ ATIVIDADES_ESTUDO/UNIDADE02/Q11/Program.cs
@@ -8,6 +8,17 @@
     */
     private static void Main(string[] args)
     {
-    
+        double imc, altura, peso;
+
+        Console.Write("Informe a sua altura: ");
+        altura = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Informe o seu peso: ");
+        peso = Convert.ToDouble(Console.ReadLine());
+
+        /*imc = (altura * altura)/peso;*/
+        imc = peso / Math.Pow(altura,2);
+
+        Console.WriteLine("IMC: {0:N3}",imc);
     }
 }
---
+++ VIANNA_EM_SALA/02/Program.cs
@@ -0,0 +1,50 @@
+﻿internal class Program
+{
+
+    static void imprimirSoma(int numero1,int numero2){
+        int soma;
+        soma = Somar(numero1,numero2);
+        Console.WriteLine("Soma: {0}",soma);
+    }
+
+    static int Somar(int numero1,int numero2){
+        int soma;
+        soma = numero1 + numero2;
+        return soma;
+    }
+
+
+    static void Main(string[] args){
+        
+        int num1, num2;
+        /*int result;*/
+        double celsius,fahrenheit;
+
+        Console.Write("Primeiro número: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Segundo número: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Informe a temperatura em Celcius: ");
+        celsius = Convert.ToDouble(Console.ReadLine());
+
+        imprimirSoma(num1,num2);
+        
+        /*Somar(10,25);
+
+        result = Somar(num1,num2);
+        Console.WriteLine("Soma: {0}",result);
+        */
+
+        fahrenheit = CelsiusFahrenheit(celsius);
+        Console.WriteLine("Temperatura em Fahrenheit: {0}",fahrenheit);
+        Console.ReadKey();''
+    }
+
+    static double CelsiusFahrenheit(double tc){
+        double tf;
+        tf = 1.8 * tc + 32;
+        return tf;
+    }
+}
