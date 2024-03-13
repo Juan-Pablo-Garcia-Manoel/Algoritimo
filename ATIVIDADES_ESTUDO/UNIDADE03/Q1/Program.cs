220d91afde22351b821bec23c5a9b16380a64491
JuanPabloGarciaManoel <79871430+JuanPabloGarciaManoel@users.noreply.github.com>
2024-03-13 23:11:13 +0000

    Estudo

---
+++ ATIVIDADES_ESTUDO/UNIDADE03/Q1/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    /*
+        
+    */
+    static void media(double n1, double n2, double n3){
+        double media;
+        media = (n1 + n2 + n3)/3.0;
+        Console.WriteLine("Média: {0}", media);
+    }
+
+    private static void Main(String[] args){
+        double n1,n2,n3;
+        Console.Write("Informe o valor de n1: ");
+        n1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Informe o valor de n2: ");
+        n2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Informe o valor de n3: ");
+        n3 = Convert.ToDouble(Console.ReadLine());
+        media(n1,n2,n3);
+    }
+}
---
+++ ATIVIDADES_ESTUDO/UNIDADE03/Q2/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+
+    /*
+    
+    */
+    static double desconto(double valor, double descontoProduto){
+        double resultado;
+        resultado = (valor - (valor * (descontoProduto/100))); 
+        return resultado;
+    }
+
+    private static void Main(string[] args)
+    {
+        double valor;
+       double descontoProduto;
+       double resultado;
+
+       Console.WriteLine("Informe o valor do produto: ");
+       valor = Convert.ToDouble(Console.ReadLine());
+
+       Console.WriteLine("Informe a porcentagem de desconto do produto: ");
+       descontoProduto = Convert.ToDouble(Console.ReadLine());
+
+       resultado = desconto(valor,descontoProduto);
+       Console.WriteLine("Valor descontado: {0}",resultado);
+    }
+}
