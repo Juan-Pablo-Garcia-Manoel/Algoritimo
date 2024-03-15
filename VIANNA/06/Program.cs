0bd4fbda62f6cd5861b2496a5e390c41a77c0cdc
JuanPabloGarciaManoel <79871430+JuanPabloGarciaManoel@users.noreply.github.com>
2024-03-15 01:30:07 +0000

    Estudo

---
+++ VIANNA/06/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*DECLARAÇÃO*/
+        int[] idade = new int[2];
+        int calculo;
+        /*ATRIBUINDO VALORES*/
+        idade[0] = 22;
+        idade[1] = 25;
+        calculo = idade[0] - idade[1];
+
+        /*IMPRIMINDO*/
+
+        Console.WriteLine("{0} e {1} e {2}", idade[0],idade[1],calculo);
+    }
+} 
