ed0097f7aedcc5a4379cca6fa1f3a8867d406d31
JuanPabloGarciaManoel <79871430+JuanPabloGarciaManoel@users.noreply.github.com>
2024-05-20 23:15:14 +0000

    Estudo

--- ATIVIDADES_ESTUDO/UNIDADE07/00/Program.cs
+++ ATIVIDADES_ESTUDO/UNIDADE07/00/Program.cs
@@ -17,14 +17,14 @@ class Program
         Console.Write("Digite um número inteiro: ");
         n = Convert.ToInt32(Console.ReadLine());
 
-        int i = 1;
-        while(i < n + 1){
+        int i = 2;
+        while(i <= n){
             e = 1 + (1/i);
             resultado += e;
-            i ++;
+            i++;
         }
         
-        Console.WriteLine("Resultado: {0}", resultado);
+        Console.WriteLine("Resultado: {0:N2}", resultado);
 
         /*if(n < 0){
             Console.ReadLine("Número inválido ");        
---
+++ VIANNA_EM_SALA/07/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _07;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i,j;
+                                /*3 Linhas e 4 colunas, está matriz possui 12 elementos*/
+        int[,] matriz = new int[3,4];
+        
+
+        for(i = 0; i < 3; i++){ /*Pecorre todas as colunas*/
+            for(j = 0; j < 4; j++){ /*para cada linha*/
+                matriz[i,j] = 0; /***/
+            }
+        }
+
+        Console.Write("Matriz:\n");
+        for(i = 0; i < 3; i++){ 
+            Console.Write("\nLinha {0}\n",i);
+            for(j = 0; j < 4; j++){ 
+                Console.Write("{0}\t",matriz[i,j]);
+            }
+        }
+
+    }
+}
